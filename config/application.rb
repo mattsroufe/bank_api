1e85f72f74b86d17227e1c8a13714254c9401816
Matt Sroufe <mattsroufe@Matts-MacBook-Pro.local>
2014-10-08 21:32:24 +1300

    allows cross origin request

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,8 @@ module BankApi
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.action_dispatch.default_headers.merge!({
+      'Access-Control-Allow-Origin' => 'http://127.0.0.1'
+    })
   end
 end
